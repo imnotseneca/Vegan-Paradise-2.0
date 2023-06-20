9c83baf00bfdda185c4f8f8d113a972011eff5c3
BrunoC <corraobruno13@gmail.com>
2023-06-20 14:33:04 -0300

    various style changes

--- src/components/Hero-section/MainSection.jsx
+++ src/components/Hero-section/MainSection.jsx
@@ -5,7 +5,7 @@ export default function MainSection () {
     return (
         <main>
         <Card />
-        <img src="https://res.cloudinary.com/dd8ikgzov/image/upload/v1686597564/Vegan-Paradise/vegan-pizza_ks24tg.png" alt="Pizza-image" className='pizza-image' />
+        {/* <img src="https://res.cloudinary.com/dd8ikgzov/image/upload/v1686597564/Vegan-Paradise/vegan-pizza_ks24tg.png" alt="Pizza-image" className='pizza-image' /> */}
         </main>
     )
 }
--- src/data/processData.js
+++ src/data/processData.js
@@ -1,25 +1,25 @@
 export const processData = [
     {
         id: 1,
-        imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1686597559/Vegan-Paradise/mobile-order_spjwad.png',
+        imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1687280520/Vegan-Paradise/undraw_online_groceries_a02y_e8wvnt.svg',
         alt: 'Place-order',
         processText: 'Place your order',
     },
     {
         id: 2,
-        imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1686597559/Vegan-Paradise/mobile-order_spjwad.png',
+        imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1687280520/Vegan-Paradise/undraw_cooking_p7m1_yhkwxt.svg',
         alt: 'Order-preparation',
         processText: 'Order preparation',
     },
     {
         id: 3,
-        imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1686868789/Vegan-Paradise/fast-dlivery_ecqryn.svg',
+        imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1687280520/Vegan-Paradise/undraw_order_delivered_re_v4ab_oecle0.svg',
         alt: 'Order-delivery',
         processText: 'Order Delivery',
     },
     {
         id: 4,
-        imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1686597559/Vegan-Paradise/mobile-order_spjwad.png',
+        imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1687280577/Vegan-Paradise/undraw_pizza_sharing_wxop_lpacdh.svg',
         alt: 'Order-received',
         processText: 'Order received',
     }
