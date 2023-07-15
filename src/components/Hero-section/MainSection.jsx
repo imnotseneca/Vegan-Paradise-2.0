bdb48706ad7c6b884766a42f5ad3ccb53755c80e
BrunoC <corraobruno13@gmail.com>
2023-07-15 12:50:58 -0300

    trying to reduce LCP

--- src/components/Hero-section/MainSection.jsx
+++ src/components/Hero-section/MainSection.jsx
@@ -4,8 +4,6 @@ import './mainsection.css'
 export default function MainSection () {
     return (
         <main>
-        {/* <Card /> */}
-        {/* <img src="https://res.cloudinary.com/dd8ikgzov/image/upload/v1686597564/Vegan-Paradise/vegan-pizza_ks24tg.png" alt="Pizza-image" className='pizza-image' /> */}
         </main>
     )
 }
