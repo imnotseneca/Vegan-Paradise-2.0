69117e15362cf329686a77089cbaf74c0aa48c85
BrunoC <corraobruno13@gmail.com>
2023-07-18 20:35:30 -0300

    Changes on Service layout and process Data. Ended order flow with wpp API.

--- src/components/Service-section/Service.jsx
+++ src/components/Service-section/Service.jsx
@@ -1,15 +1,15 @@
-import './service.css'
-import Process from "./Process"
-import {processData} from '../../data/processData'
+import "./service.css";
+import Process from "./Process";
+import { processData } from "../../data/processData";
 
-export default function Service () {
-    return (
-        <section id="service" className="service-section">
-            <h2 className="service-header">Service process</h2>
-            <div className="process-container">
-            <Process 
-                processData={processData}/>
-            </div>
-        </section>
-    )
+export default function Service() {
+  return (
+    <section id="service" className="service-section">
+      <div className="service-header-container">
+        <h2 className="service-header">Service process</h2>
+        <span className="service-text">How it works</span>
+      </div>
+      <Process processData={processData}></Process>
+    </section>
+  );
 }
--- src/data/processData.js
+++ src/data/processData.js
@@ -1,26 +1,34 @@
 export const processData = [
     {
-        id: 1,
+        id: 0.1,
+        idForText:5,
         imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1687280520/Vegan-Paradise/undraw_online_groceries_a02y_e8wvnt.svg',
         alt: 'Place-order',
-        processText: 'Navigate through our menu and place your order.',
+        processHeader: 'Navigate through our menu and place your order.',
+        processText: `When completiting the order on whatsapp don't forget to put an adress and your fullname.`,
     },
     {
-        id: 2,
+        id: 0.2,
+        idForText:6,
         imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1687280520/Vegan-Paradise/undraw_cooking_p7m1_yhkwxt.svg',
         alt: 'Order-preparation',
-        processText: 'Step back and chill while the order is prepared.',
+        processHeader: 'Step back and chill while the order is prepared.',
+        processText: 'It takes some time to craft delicious and nourishing food. You can keep about us meanwhile!',
     },
     {
-        id: 3,
+        id: 0.3,
+        idForText:7,
         imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1687280520/Vegan-Paradise/undraw_order_delivered_re_v4ab_oecle0.svg',
         alt: 'Order-delivery',
-        processText: 'We promise to deliver within 30-50 minutes.',
+        processHeader: 'We promise to deliver within 30-50 minutes.',
+        processText: 'You should take in mind your location and how far our delivers must travel.',
     },
     {
-        id: 4,
+        id: 0.4,
+        idForText:8,
         imageURL:'https://res.cloudinary.com/dd8ikgzov/image/upload/v1687280577/Vegan-Paradise/undraw_pizza_sharing_wxop_lpacdh.svg',
         alt: 'Order-received',
-        processText: 'Enjoy your paradisiac food.',
+        processHeader: 'Enjoy your paradisiac food.',
+        processText: `Once arrived, you won't belive we prepared for you, instead you can prepare to delighted`,
     }
 ]
